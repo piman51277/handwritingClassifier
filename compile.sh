71e2144657e61d5f85a94748e1ae94c9859697e8
piman51277 <piman.dev@gmail.com>
2024-07-18 03:47:24 -0400

    use var

--- compile.sh
+++ compile.sh
@@ -2,7 +2,9 @@
 export CXXFLAGS="-Ofast -march=native -flto -funroll-loops -flto -fprefetch-loop-arrays"
 export CUDAFLAGS="-std=c++20 -O3 -diag-suppress 186"
 
+#tools
+#g++ tools/generateMatrix.cpp $CXXFLAGS -o generateMatrix
 
-#g++ tools/generateMatrix.cpp -Ofast -march=native -flto -funroll-loops -flto -fprefetch-loop-arrays -o generateMatrix
+#main script
 g++ src/*.cpp $CXXFLAGS -o bin/classifier
 #nvcc src/*.cu -O3 -diag-suppress 186 -gencode=arch=compute_86,code=sm_86 -o bin/a.out

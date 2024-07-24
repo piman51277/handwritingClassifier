aaddb1fbcccfc49869b8a3a57082bb12c69c2139
piman51277 <piman@piman.dev>
2024-07-24 19:15:49 -0400

    add AOCC option

--- compile.sh
+++ compile.sh
@@ -1,12 +1,15 @@
 
-export CXXFLAGS="-Ofast -march=native -flto -funroll-loops -flto -fprefetch-loop-arrays"
+export CXXFLAGS="-Ofast -march=native -flto -funroll-loops -flto"
 export CUDAFLAGS="-std=c++20 -O3 -diag-suppress 186"
 
 #tools
-#g++ tools/generateMatrix.cpp $CXXFLAGS -o generateMatrix
+g++ tools/generateMatrix.cpp $CXXFLAGS -o generateMatrix
+
+#set AMD flags
+source /opt/AMD/aocc-compiler-4.2.0/setenv_AOCC.sh
 
 #main script
 echo "Compiling CPU..."
-g++ src/cpu/*.cpp $CXXFLAGS -o bin/classifier_baseline
+clang++ src/cpu/*.cpp $CXXFLAGS -o bin/classifier_baseline
 echo "Compiling GPU..."
 nvcc src/gpu/*.cu -O3 -diag-suppress 186 -gencode=arch=compute_86,code=sm_86 -o bin/classifier
